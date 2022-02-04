468612358dda5f911558b300b84d9e56cbdb2682
Younis Rahman <younisrahman007@gmail.com>
2022-02-04 22:35:15 +0600

    next roound

--- NextRound158A.py
+++ NextRound158A.py
@@ -1,4 +1,9 @@
 n, k = map(int, input().split())
 a = list(map(int, input().strip().split()))[:n]
+count = 0
 
-print(a[k])
+for i in range(n):
+    if(a[i] >= a[k] and a[i] != 0):
+        count += 1
+
+print(count)
