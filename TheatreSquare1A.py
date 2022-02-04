ed66bedc0b2a8db4388905ddb5d22b58ae29c0b9
Younis Rahman <younisrahman007@gmail.com>
2022-02-04 14:08:06 +0600

    test

--- TheatreSquare1A.py
+++ TheatreSquare1A.py
@@ -3,24 +3,32 @@
 # print(n[0] % 4 == 0)
 # print(n[0] // (4 + 1))
 
-n, m, a = map(int, input().split())
+# n, m, a = map(int, input().split())
+# if m % a == 0:
+#     r1 = m//a
+#     print("if", r1)
+# else:
+#     r1 = m//a+1
+#     print("else", r1)
+# if n % a == 0:
+#     r2 = n//a
+#     print("if", r2)
+# else:
+#     r2 = n//a+1
+#     print("else", r2)
 
-if m % a == 0:
-    r1 = m//a
-    print("if", r1)
+# print(r1*r2)
 
+n = list(map(int, input().strip().split()))[:3]
+
+if n[0] % n[2] == 0:
+    t1 = n[0]//n[2]
 else:
-    r1 = m//a+1
-    print("else", r1)
+    t1 = n[0]//n[2]+1
 
-
-if n % a == 0:
-    r2 = n//a
-    print("if", r2)
-
+if n[1] % n[2] == 0:
+    t2 = n[1]//n[2]
 else:
-    r2 = n//a+1
-    print("else", r2)
+    t2 = n[1]//n[2]+1
 
-
-print(r1*r2)
+print(t1*t2)
