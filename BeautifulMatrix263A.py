ae6804d310ee112bd2f5df964d47c6c9a47be48b
Younis Rahman <younisrahman007@gmail.com>
2022-02-06 00:04:14 +0600

    263a

--- BeautifulMatrix263A.py
+++ BeautifulMatrix263A.py
@@ -8,7 +8,7 @@ for _ in range(5):
         # print(num)
         arr2.append(num)
         if num == 1:
-            print("SS")
+            # print("SS")
             arrnum.append(_)
             arrnum.append(i)
     arr.append(arr2)
@@ -17,6 +17,6 @@ for _ in range(5):
 #     arr.append(list(map(int, input().rstrip().split())))
 
 beaatufilNumber = abs(arrnum[0]-2) + abs(arrnum[1]-2)
-print(arr)
-print(arrnum)
+# print(arr)
+# print(arrnum)
 print(beaatufilNumber)
