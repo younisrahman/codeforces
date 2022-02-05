e25856e7fdb05736fe49aa220a9d15774463ae87
Younis Rahman <younisrahman007@gmail.com>
2022-02-05 23:54:56 +0600

    263A

---
+++ BeautifulMatrix263A.py
@@ -0,0 +1,21 @@
+arr = []
+# arrnum = []
+# for _ in range(5):
+#     arr2 = []
+#     n = input()
+#     for i in range(5):
+#         num = n.strip().split()[i]
+#         # print(num)
+#         arr2.append(num)
+#         if num == 1:
+#             print("SS")
+#             arrnum.append(_)
+#             arrnum.append(i)
+#     arr.append(arr2)
+
+# for _ in range(6):
+#     arr.append(list(map(int, input().rstrip().split())))
+
+
+print(arr)
+# print(arrnum)
