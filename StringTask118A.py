55a72a66f2e65611b80317dc3d7987397e696e72
Younis Rahman <younisrahman007@gmail.com>
2022-02-07 11:09:17 +0600

    Help

--- StringTask118A.py
+++ StringTask118A.py
@@ -1,4 +1,4 @@
 s = input().lower()
 for l in s:
-    if l != 'a' and l != 'e' and l != 'i' and l != 'o' and l != 'u':
-        print('.', l, end='')
+    if l != 'a' and l != 'e' and l != 'i' and l != 'o' and l != 'u' and l != 'y':
+        print(f".{l}", end='')
