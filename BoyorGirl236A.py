9e5ab0e745a6ff3e5f28ccf2db77fe22238dfbbb
Younis Rahman <younisrahman007@gmail.com>
2022-02-18 22:19:06 +0600

    236A

--- BoyorGirl236A.py
+++ BoyorGirl236A.py
@@ -1,6 +1,7 @@
-from operator import indexOf
-
-
-n = input()
-# for i in n:
-print(indexOf('a'))
+s = input()
+for i in s:
+    s.pop(0)
+    if s.find(i) == -1:
+        print(f"No {i} here!")
+    else:
+        print(f"Found {i} in the string.")
