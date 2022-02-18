0ca826492c0e511322509209ed88ffc39c271e19
Younis Rahman <younisrahman007@gmail.com>
2022-02-18 19:28:35 +0600

    236a

---
+++ BoyorGirl236A.py
@@ -0,0 +1,6 @@
+from operator import indexOf
+
+
+n = input()
+# for i in n:
+print(indexOf('a'))
---
+++ WordCapitalization281A.py
@@ -0,0 +1,5 @@
+n = input()
+# print(n.capitalize())
+rest = n[1:]
+first = n[0].upper()
+print(first+rest)
