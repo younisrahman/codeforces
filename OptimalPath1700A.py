e4f0e647818bbd4b9f4688e77f997b056780f131
Younis <younisrahman007@gmail.com>
2022-06-30 00:47:27 +0600

    1700a time

--- OptimalPath1700A.py
+++ OptimalPath1700A.py
@@ -1,9 +1,10 @@
 a = int(input())
-for i in range(a):
+while (a != 0):
     n, m = map(int, input().split())
     ans = (m*(m+1))/2
     for j in range(2, n+1):
         ans += ((j-1)*m)+m
     print(int(ans))
+    a -= 1
 
 # print(a, n, m)
