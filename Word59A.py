2c83d336df6768daf4f56484d8bd4c5112ec0ad0
Younis <younisrahman007@gmail.com>
2022-07-01 17:24:51 +0600

    functuon

--- Word59A.py
+++ Word59A.py
@@ -1,2 +1,10 @@
 s = input()
-print(s)
+capital = 0
+small = 0
+for i in s:
+    if i.isupper():
+        capital += 1
+    else:
+        small += 1
+
+print(s.upper() if capital > small else s.lower())
