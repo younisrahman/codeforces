f85032bd5ec86a4078a4c025d3ab14e1be687334
Younis <younisrahman007@gmail.com>
2022-07-01 16:49:52 +0600

    add array

--- YoungPhysicist69A.py
+++ YoungPhysicist69A.py
@@ -1,1 +1,11 @@
+n = int(input())
+x, y, z = [], [], []
+for i in range(n):
+    a, b, c = map(int, input().split())
+    x.append(a)
+    y.append(b)
+    z.append(c)
 
+print(x)
+print(y)
+print(z)
