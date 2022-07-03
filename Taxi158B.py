57c5d6c104836fdab2fe0d37da97433e8a8ebb27
Younis <younisrahman007@gmail.com>
2022-07-03 13:16:28 +0600

    158B Accepted

--- Taxi158B.py
+++ Taxi158B.py
@@ -1,19 +1,47 @@
 n = int(input())
+# i = input()
 one = 0
 two = 0
 three = 0
 four = 0
-for i in range(n):
-    a = int(input())
-    if a == 1:
+for i in input().split()[:n]:
+    i = int(i)
+    if i == 1:
         one += 1
-    elif a == 2:
+    elif i == 2:
         two += 1
-    elif a == 3:
+    elif i == 3:
         three += 1
-    elif a == 4:
+    elif i == 4:
         four += 1
-print(one)
-print(two)
-print(three)
-print(four)
+
+count = four
+if three == one:
+    count += one
+    three = one = 0
+elif three < one:
+    count += three
+    one = one - three
+    three = 0
+elif three > one:
+    count += three
+    three = one = 0
+
+if two % 2 == 0:
+    count += two//2
+    two = 0
+elif two % 2 == 1:
+    count += two//2
+    two = 1
+if two == 1:
+    one -= 2
+    count += 1
+if one > 0:
+
+    if one % 4 == 0:
+        count += one//4
+        one = 0
+    elif one % 4 != 0:
+        count += (one//4)+1
+
+print(count)
