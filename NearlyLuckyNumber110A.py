9bf56dc59293a00635d274632b6b89e8c2af5620
Younis <younisrahman007@gmail.com>
2022-07-03 15:56:01 +0600

    110A accepted

--- NearlyLuckyNumber110A.py
+++ NearlyLuckyNumber110A.py
@@ -1,7 +1,8 @@
-n = int(input())
+n = input()
 count = 0
 for i in n:
-    if i == 4 and i == 7:
+    i = int(i)
+    if i == 4 or i == 7:
         count += 1
 
 print("YES" if count == 4 or count == 7 else "NO")
