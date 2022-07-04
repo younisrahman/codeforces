b1b4a69917c0b541b423b7389d6ebec040ef5586
Younis <younisrahman007@gmail.com>
2022-07-04 22:04:41 +0600

    Chat room

--- Chatroom58A.py
+++ Chatroom58A.py
@@ -1,3 +1,21 @@
 s = input()
+count = 0
+str = ''
 for i in s:
-    print(i, end="")
+    if count == 0 and i == 'h':
+        str = str + i
+        count = count + 1
+    elif count == 1 and i == 'e':
+        str = str + i
+        count = count + 1
+    elif count == 2 and i == 'l':
+        str = str + i
+        count = count + 1
+    elif count == 3 and i == 'l':
+        str = str + i
+        count = count + 1
+    elif count == 4 and i == 'o':
+        str = str + i
+        count = count + 1
+
+print("Yes" if str == 'hello' else "No")
