40b8140c2800fd739eb27a57774dea77e12f46b7
Younis <younisrahman007@gmail.com>
2022-07-04 22:12:18 +0600

    Anton and Danik

--- AntonAndDanik734A.py
+++ AntonAndDanik734A.py
@@ -1,1 +1,16 @@
 # Anton and Danik
+n = int(input())
+s = input()
+A = 0
+D = 0
+for i in s:
+    if i == 'A':
+        A += 1
+    elif i == 'D':
+        D += 1
+if A > D:
+    print("Anton")
+elif A < D:
+    print("Danik")
+else:
+    print("Friendship")
