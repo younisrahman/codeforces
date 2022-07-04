cf75c223c6b92221565bce6c65374fb436b83a90
Younis <younisrahman007@gmail.com>
2022-07-04 12:48:41 +0600

    com[lete 266B

--- QueueAtTheSchool266B.py
+++ QueueAtTheSchool266B.py
@@ -1,5 +1,23 @@
 n, s = map(int, input().split())
 queue = input()
+output = ''
 for i in range(s):
-    for j in range(len(queue)-1):
-        print(queue[j])
+    count = 0
+    # queue = output
+    output = ''
+    while(n > count):
+        try:
+            if queue[count] == "B" and queue[count+1] == "G":
+                output += "GB"
+                count += 2
+            else:
+                output += "G"
+                count += 1
+        except:
+            output += queue[count]
+            count += 1
+
+    queue = output
+
+
+print(queue)
