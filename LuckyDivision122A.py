3fb7109a5ee22377c6c9975b84df171524a3bc9e
Younis <younisrahman007@gmail.com>
2022-07-04 23:07:01 +0600

    # LuckyDivision122A.py n = int(input()) if n % 4 == 0 or n % 7 == 0:     print("YES") else:     s = NO     n = str(n)     print(n)     for i in n:
            s = "NO"
            if i == "4" or i == 7:
                s = "YES"
        print(s)

--- LuckyDivision122A.py
+++ LuckyDivision122A.py
@@ -1,1 +1,14 @@
 # LuckyDivision122A.py
+n = int(input())
+if n % 4 == 0 or n % 7 == 0:
+    print("YES")
+else:
+    s = 'NO'
+    n = str(n)
+    print(n)
+    for i in n:
+
+        s = "NO"
+        if i == "4" or i == '7':
+            s = "YES"
+    print(s)
