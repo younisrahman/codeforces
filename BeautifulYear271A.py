8433be3128904e4e0dc719375c40d0eddc9e8eb2
Younis <younisrahman007@gmail.com>
2022-07-05 17:10:56 +0600

    Beautiful Year

--- BeautifulYear271A.py
+++ BeautifulYear271A.py
@@ -1,1 +1,14 @@
 # Beautiful Year
+n = int(input()) + 1
+loop = True
+while loop:
+    year = str(n)
+    yearSet = set()
+    for i in year:
+        yearSet.add(i)
+    if len(yearSet) == 4:
+        loop = False
+    else:
+        n = n + 1
+
+print(n)
