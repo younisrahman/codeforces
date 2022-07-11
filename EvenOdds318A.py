ccd3f282484f6fa8b56e9e6a4b611e48072d74e0
Younis <younisrahman007@gmail.com>
2022-07-12 03:46:14 +0600

    np right

--- EvenOdds318A.py
+++ EvenOdds318A.py
@@ -1,12 +1,25 @@
+# n, k = map(int, input().split())
+# odd = []
+# even = []
+# for i in range(n):
+#     if i+1 == 1:
+#         odd.append(i+1)
+#     elif (i+1) % 2 == 0:
+#         even.append(i+1)
+#     else:
+#         odd.append(i+1)
+# array = odd + even
+# print(array[k-1])
 n, k = map(int, input().split())
-odd = []
-even = []
-for i in range(n):
-    if i+1 == 1:
-        odd.append(i+1)
-    elif (i+1) % 2 == 0:
-        even.append(i+1)
+a = (k*2)-1
+b = 0
+if (a > n):
+    if n % 2 == 0:
+        b = n/2
     else:
-        odd.append(i+1)
-array = odd + even
-print(array[k-1])
+        b = n/2+1
+        k = k-b
+        b = k*2
+        print(b)
+else:
+    print(a)
