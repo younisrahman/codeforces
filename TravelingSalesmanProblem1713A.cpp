e50f4c93d7753a130a01e813bfe07fabd0b00468
Younis Rahman <younisrahman007@gmail.com>
2022-08-24 00:04:48 +0600

    Traveling Salesman Problem

--- TravelingSalesmanProblem1713A.cpp
+++ TravelingSalesmanProblem1713A.cpp
@@ -3,13 +3,20 @@
 using namespace std;
 int main()
 {
-    int n, a, b, c, d;
+    int n, a, b, c, d, e;
     vector<int> vector;
     set<int> set;
     cin >> n;
     while (n--)
     {
-        cin >> a >> b >> c >> d;
+        cin >> a;
+        int sum = 0;
+        for (int i = 0; i < a; i++)
+        {
+            cin >> b >> c;
+            sum += abs(b) + abs(c);
+        }
+        std::cout << sum * 2 << std::endl;
     }
     return 0;
 }
