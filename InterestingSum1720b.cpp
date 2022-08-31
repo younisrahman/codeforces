139043f23ccfd2b04f9c5a9ac21649ed2237cc87
Younis Rahman <younisrahman007@gmail.com>
2022-09-01 01:39:04 +0600

    sum

---
+++ InterestingSum1720b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+int main()
+{
+    int n, a, b, c, d;
+    set<int> set;
+    cin >> n;
+    while (n--)
+    {
+        cin >> a;
+        c = a;
+        vector<int> v;
+        while (c--)
+        {
+            cin >> b;
+            v.push_back(b);
+        }
+        sort(v.begin(), v.end());
+        cout << (v[a - 1] + v[a - 2]) - (v[0] + v[1]) << endl;
+    }
+    return 0;
+}
