4f118dbfdd751a60dcb44d0333439aec53971747
Younis Rahman <younisrahman007@gmail.com>
2022-09-06 14:23:17 +0600

    min max

--- Min-MaxArrayTransformation1721C.cpp
+++ Min-MaxArrayTransformation1721C.cpp
@@ -9,27 +9,28 @@ int main()
     while (n--)
     {
         cin >> a;
-        vector<int> veca[a];
-        vector<int> vecb[a];
+        vector<int> veca;
+        vector<int> vecb;
         for (int i = 0; i < a; i++)
         {
             cin >> b;
-            veca->push_back(b);
+            veca.push_back(b);
         }
         for (int i = 0; i < a; i++)
         {
             cin >> c;
-            vecb->push_back(c);
+            vecb.push_back(c);
         }
 
-        for (int i = 0; i < a; i++)
-        {
-            std::cout << veca[i] << "  " << std::endl;
-        }
-        for (int i = 0; i < a; i++)
-        {
-            std::cout << vecb[i] << "  " << std::endl;
-        }
+        // for (int i = 0; i < a; i++)
+        // {
+        //     std::cout << veca[i] << "  ";
+        // }
+        // std::cout << std::endl;
+        // for (int i = 0; i < a; i++)
+        // {
+        //     std::cout << vecb[i] << "  ";
+        // }
     }
     return 0;
 }
